17e9674f5e2b4a84b587a117c92f7ebd6d23e798
mariam606 <67857016+mariam606@users.noreply.github.com>
2023-06-26 01:02:24 +0300

    Update dsu_on_trees.cpp

--- dsu_on_trees.cpp
+++ dsu_on_trees.cpp
@@ -1,56 +1,63 @@
 // DSU on trees: this code is used to implement euler tour algorithm on a tree and convert it into an array (the vertex array). The vertex array depends
-// on the tin o each vertex because it's unique. After that, you can update a property (e.g. most frequent element) about a subtree on log using the below dfs. 
+// on the tin o each vertex because it's unique. It is only used in case of offline queries
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
  
-int n;
+int n, q, timer;
 const int N = 1e5 + 5;
 vector<int> g[N];
-int a[N], tin[N], tout[N], sz[N], vertex[N];
-ll ans[N];
-int timer;
- 
-void add(int property){ 
+int a[N], tin[N], tout[N], sz[N], vertex[N], p[N], depth[N];
+vector<vector<int>> query_idx;
+
+void add(int v) {
 
 }
- 
-void erase(int property){
+
+void erase(int v) {
 
 }
- 
-void solve_queries(int node){
+
+void solve_queries(int v) {
+
 }
- 
-int pre(int node, int par){
-	tin[node] = ++timer;
-	vertex[tin[node]] = node;
-	sz[node] = 1;
-	for(auto child: g[node])
-		if(child != par) sz[node] += pre(child, node);
- 
-	tout[node] = timer;
-	return sz[node];
+
+int pre(int v) {
+    tin[v] = ++timer;
+    vertex[tin[v]] = v;
+    sz[v] = 1;
+    for (auto& u : g[v]) if (u != p[v]) {
+        depth[u] = depth[v] + 1;
+        p[u] = v;
+        sz[v] += pre(u);
+    }
+    tout[v] = timer;
+    return sz[v];
 }
- 
-void dfs(int v, int p, bool keep) {
+
+void dfs(int v, bool keep) {
     int big_child = -1, big_size = 0;
-    for (auto& u : g[v]) if (u != p && sz[u] > big_size) {
+    for (auto& u : g[v]) if (u != p[v] && sz[u] > big_size) {
         big_child = u;
         big_size = sz[u];
     }
-    for (auto& u : g[v]) if (u != p && u != big_child) dfs(u, v, 0);
-    if (big_child != -1) dfs(big_child, v, 1);
-    for (auto& u : g[v]) if (u != p && u != big_child) 
-        for (int i = tin[u]; i <= tout[u]; i++) add(a[vertex[i]]);
-    add(a[v]);
+    for (auto& u : g[v]) if (u != p[v] && u != big_child) dfs(u, 0);
+    if (big_child != -1) dfs(big_child, 1);
+    for (auto& u : g[v]) if (u != p[v] && u != big_child) 
+        for (int i = tin[u]; i <= tout[u]; i++) add(vertex[i]);
+    add(v);
     solve_queries(v);
-    if (!keep) for (int i = tin[v]; i <= tout[v]; i++) erase(a[vertex[i]]);
+    if (!keep) for (int i = tin[v]; i <= tout[v]; i++) erase(vertex[i]);
+}
+
+bool is_ancestor(int u, int v) {
+    return (tin[u] <= tin[v] && tin[v] <= tout[u]);
 }
  
 void solve() {
-	pre(1, -1);
-	dfs(1, -1, 0);
+    timer = 0;
+	pre(1);
+	dfs(1, 0);
 }
  
 int main() {
