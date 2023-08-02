93cb9751dc798d2c0fad8712f0362e02a3764889
mariam606 <67857016+mariam606@users.noreply.github.com>
2023-08-02 20:08:15 +0300

    Update pieces_of_code.cpp

--- pieces_of_code.cpp
+++ pieces_of_code.cpp
@@ -8,5 +8,7 @@ auto get_id = [&](int x) {
  return comp.emplace(x, (int)comp.size()).first->second;
 };
 
-
+//looping over the submasks of the mask s
+for (int msk = s; msk; msk =(msk - 1) & s)
+    ..use the msk
 
