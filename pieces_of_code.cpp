67f9ff6eb3910751404d97badd88f8e6602f256f
mariam606 <67857016+mariam606@users.noreply.github.com>
2023-08-03 20:10:08 +0300

    Update pieces_of_code.cpp

--- pieces_of_code.cpp
+++ pieces_of_code.cpp
@@ -17,3 +17,8 @@ int get_lcm(int x, int y) { return (x / __gcd(x, y)) * y; }
 
 //Number of subrectangles in a rectangle n * m
 n * (n + 1) * m * (m + 1) / 4
+
+//Stress testing
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rand(int a, int b) { return uniform_int_distribution<int>(a, b)(rng); }
+ 
