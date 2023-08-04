bf816ac71d5cfbf3364c71ad4dcbf3a7d3f39c5f
mariam606 <67857016+mariam606@users.noreply.github.com>
2023-08-04 16:48:35 +0300

    Update pieces_of_code.cpp

--- pieces_of_code.cpp
+++ pieces_of_code.cpp
@@ -21,4 +21,7 @@ n * (n + 1) * m * (m + 1) / 4
 //Stress testing
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rand(int a, int b) { return uniform_int_distribution<int>(a, b)(rng); }
+
+//pi
+double pi = 1.0 * acos(-1);
  
