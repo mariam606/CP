8c44257dba4e333785aefb1573904755e0c36602
mariam606 <67857016+mariam606@users.noreply.github.com>
2023-08-06 04:06:30 +0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+using namespace std;
+
+void solve() {
+
+}
+ 
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    //cout.flush();
+    // freopen("max-pair.in", "r", stdin)
+    int T = 1;
+    cin >> T;
+    while (T--) solve(); 
+}
