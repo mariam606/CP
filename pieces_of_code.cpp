b4d5dea6a04217f26daca3dfb9836d95aa8abf3f
mariam606 <67857016+mariam606@users.noreply.github.com>
2023-08-06 16:21:25 +0300

    Update pieces_of_code.cpp

--- pieces_of_code.cpp
+++ pieces_of_code.cpp
@@ -4,7 +4,7 @@ int id = 1;
 for (auto& u : comp) u.second = id++;
 
 //giving ID
-auto get_id = [&](int x) {
+function<int(int)> get_id = [&](int x) {
  return comp.emplace(x, (int)comp.size()).first->second;
 };
 
