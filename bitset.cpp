ec8dbbe86fc792e612dd7890c3d279380500e148
mariam606 <67857016+mariam606@users.noreply.github.com>
2023-08-06 04:06:08 +0300

    Create bitset.cpp

---
+++ bitset.cpp
@@ -0,0 +1,26 @@
+//Bitset can be used to solve the subset problem. It can same all the possible subset sums of an array in O(s * n / 64) where s is the max sum and n is the size of the array. 
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+const int N = 2e5 + 5;
+vector<int> a;
+
+void possible_sums(){
+  bitset<N> sums(1);
+  for (auto i: a)
+    sums |= sums << i;
+}
+
+void solve() {
+  possible_sums();
+}
+ 
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    //cout.flush();
+    // freopen("max-pair.in", "r", stdin)
+    int T = 1;
+    cin >> T;
+    while (T--) solve(); 
+}
